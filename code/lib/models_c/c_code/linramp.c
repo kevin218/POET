37ab9aa4701cf68ddc23c4495c510407252893bf
Jonathan Fraine <exowanderer@users.noreply.github.com>
2018-12-03 12:43:59 +0200

    updated docstring

--- code/lib/models_c/c_code/linramp.c
+++ code/lib/models_c/c_code/linramp.c
@@ -66,6 +66,9 @@ static char linramp_doc[] = "\
   2010-12-25    Nate Lust, UCF\n\
                 natelust at linux dot com\n\
                 Converted to C\n\
+  2018-11-11    Jonathan Fraine, SSI\n\
+                jfraine at spacescience.org\n\
+                Updated c extensions to python3, with support for python2.7\n\
 ";
 
 
