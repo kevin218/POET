73eab67c8ee3f664d404b7d42716fe8e841a44f2
Jonathan Fraine <exowanderer@users.noreply.github.com>
2018-12-03 12:45:26 +0200

    updated docstring

--- code/lib/models_c/c_code/re2ramp.c
+++ code/lib/models_c/c_code/re2ramp.c
@@ -67,6 +67,9 @@ static char re2ramp_doc[]="\
   2010-12-24    Nate Lust, UCF\n\
                 natelust at linux dot com\n\
                 Converted to C\n\
+  2018-11-11    Jonathan Fraine, SSI\n\
+                jfraine at spacescience.org\n\
+                Updated c extensions to python3, with support for python2.7\n\
 ";
 
 static PyMethodDef module_methods[] = {
